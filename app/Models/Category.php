fd12d555d466981f5d7101403ece99bcc9786c1b
JesusSePe <umbreon20019@gmail.com>
2021-02-10 20:06:56 +0100

    Relation between tasks and categories (both sides) complete

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    /**
+     * Accedeix als fills (Post)
+     */
+    public function category(){
+        return $this->hasMany('App\Models\Task', 'id');
+    }
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    /**
+     * Accedeix al pare (Post)
+     */
+    public function category(){
+        return $this->belongsTo('App\Models\Category', 'category_id');
+    }
 }
